4ebb1772dae40776f34a5597caae3e94ee3e5660
tarun-upadhyay <tarunu88@gmail.com>
2023-02-18 18:54:20 +0530

    authentication

---
+++ Scripts/mocks.js
@@ -0,0 +1,40 @@
+export const ssc = [
+  {
+    isAttempted: false,
+    finalScore: null,
+    Mock: 1,
+    questions: [
+      {
+        Question1: "How many legs have in cockroach",
+        a: 5,
+        b: 6,
+        c: 2,
+        d: 4,
+        ans: function(){
+            console.log(this)
+        }
+      }, {
+        Question1: "How many legs have in cockroach",
+        a: 5,
+        b: 6,
+        c: 2,
+        d: 4,
+        ans: 6,
+      }, {
+        Question1: "How many legs have in cockroach",
+        a: 5,
+        b: 6,
+        c: 2,
+        d: 4,
+        ans: 6,
+      }, {
+        Question1: "How many legs have in cockroach",
+        a: 5,
+        b: 6,
+        c: 2,
+        d: 4,
+        ans: 6,
+      },
+    ],
+  },
+];
--- Scripts/signup.js
+++ Scripts/signup.js
@@ -1,3 +1,48 @@
-import nav from "./navbar.js"
-document.getElementById("navbar").innerHTML = nav()
+import nav from "./navbar.js";
+document.getElementById("navbar").innerHTML = nav();
+import { ssc } from "./mocks.js";
+let userData = JSON.parse(localStorage.getItem("userData")) || [];
 
+function handleSubmit() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const exam = document.getElementById("exam").value;
+  if (exam.length < 2 || name.length < 3 || password.length < 7) {
+    if (password.length < 7) {
+      return alert("Password Length more than 8 Characters");
+    }
+    return alert(
+      "Please fill all the details and Password length minium 8 Charc"
+    );
+  }
+  let student = new User(name, email, password, exam, ssc);
+
+  let finalSubmit = userData.filter((el) => el.email === student.email);
+
+  if (finalSubmit.length) {
+    alert("You had already registred Plz login");
+    return (window.location.href = "/login.html");
+  } else {
+    userData.push(student);
+    alert("Signup Succeed");
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    return (window.location.href = "/login.html");
+  }
+}
+
+document.getElementById("form_data").addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleSubmit();
+});
+
+class User {
+  constructor(name, email, password, exam, ssc) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.exam = exam;
+    this.mocks = [ssc];
+  }
+}
