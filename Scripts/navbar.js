f63594fb33262d822451aabe69ce7b9570a2038b
tarun-upadhyay <tarunu88@gmail.com>
2023-02-18 17:40:42 +0530

    done singup sigin

---
+++ Scripts/navbar.js
@@ -0,0 +1,24 @@
+let nav = () => {
+  return `<div id="left">
+  
+      <div id="logo_nav">
+      <a href="/" style="text-decoration: none";>
+      <img src="https://images.yourstory.com/cs/images/companies/SalesKen-logo-1600327216443.jpg" alt="" id="nav_img"/>
+      </a>
+      <h3><a href="/" style="text-decoration: none";><span id="headingfirst">sale</span> academy</a></h3>
+      </div>
+    </div>
+      <div id="right">
+          <div id="login">
+          <a  href="/login.html" style="text-decoration: none";>
+          <button id="login_nav">Login</button>
+          </a>
+          </div>
+          <div id="signup">
+          <a href="/signup.html" style="text-decoration: none";>
+          <button id="singup_nav">Sign Up</button>
+          </a>
+          </div>
+      </div>`;
+};
+export default nav;
